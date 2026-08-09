460f459626eee5d50f2b128efbde7b53620641c8
Mikhail Ivanov <djgradoos@gmail.com>
2026-08-10 00:05:22 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker build -t ofer-app-flood .
 
 echo "Запуск контейнеров... Всего прокси в пуле: $NUM_PROXIES"
 
-for i in {1..100}; do 
+for i in {1..25}; do 
     docker run -d --name "ofer_$i" --restart unless-stopped ofer-app-flood; 
     echo "[$i/100] Контейнер ofer_$i запущен"
 done
